8bb21dd381f6d5a2717a7606c0ccd407aa5a2647
RobBotzz <111586693+RobBotzz@users.noreply.github.com>
2023-11-08 22:33:00 +0100

    Finished deferred rendering

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../api.js";
 import { requireAuth } from "../../utils.js";
 
@@ -7,28 +7,34 @@ import HostVanIcon from "../../components/Host/HostVanIcon.jsx";
 
 export async function loader({ request }) {
   await requireAuth(request);
-  return getHostVans();
+  return defer({ vans: getHostVans() });
+  //defer sth
 }
 
 export default function HostVans() {
-  const vansData = useLoaderData();
-
-  const vans = vansData?.map((van) => {
-    return (
-      <HostVanIcon
-        key={van.id}
-        id={van.id}
-        img={van.imageUrl}
-        name={van.name}
-        price={van.price}
-      />
-    );
-  });
+  const loaderData = useLoaderData();
 
   return (
     <div className="hostVans">
       <h2>Your listed vans</h2>
-      {vans}
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Await resolve={loaderData.vans}>
+          {(vansData) => {
+            const vans = vansData?.map((van) => {
+              return (
+                <HostVanIcon
+                  key={van.id}
+                  id={van.id}
+                  img={van.imageUrl}
+                  name={van.name}
+                  price={van.price}
+                />
+              );
+            });
+            return vans;
+          }}
+        </Await>
+      </Suspense>
     </div>
   );
 }
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,37 +1,18 @@
-import React from "react";
-import { useSearchParams, useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useSearchParams, useLoaderData, defer, Await } from "react-router-dom";
 
 import Van from "../../components/Vans/Van.jsx";
-import Loading from "../Loading.jsx";
 
 import { getVans } from "../../api.js";
 
-export async function loader() {
-  return getVans();
+export function loader() {
+  return defer({ vans: getVans() });
 }
 
 export default function Vans() {
-  const vansData = useLoaderData();
+  const loaderData = useLoaderData();
   const [searchParams, setSearchParams] = useSearchParams();
-
   const typeFilter = searchParams.get("type");
-  const filteredVansData = typeFilter
-    ? vansData?.filter((van) => van.type.toLowerCase() === typeFilter)
-    : vansData;
-
-  const vans = filteredVansData?.map((van) => {
-    return (
-      <Van
-        key={van.id}
-        searchParams={searchParams}
-        id={van.id}
-        img={van.imageUrl}
-        name={van.name}
-        price={van.price}
-        type={van.type}
-      />
-    );
-  });
 
   function handleFilterChange(key, value) {
     setSearchParams((searchParams) => {
@@ -44,40 +25,65 @@ export default function Vans() {
     });
   }
 
-  return vansData ? (
+  function renderVanElements(vansData) {
+    const filteredVansData = typeFilter
+      ? vansData?.filter((van) => van.type.toLowerCase() === typeFilter)
+      : vansData;
+
+    const vans = filteredVansData?.map((van) => {
+      return (
+        <Van
+          key={van.id}
+          searchParams={searchParams}
+          id={van.id}
+          img={van.imageUrl}
+          name={van.name}
+          price={van.price}
+          type={van.type}
+        />
+      );
+    });
+    return (
+      <>
+        <div className="filters">
+          <p
+            className={`filter simple ${typeFilter === "simple" && "selected"}`}
+            onClick={() => handleFilterChange("type", "simple")}
+          >
+            Simple
+          </p>
+          <p
+            className={`filter luxury ${typeFilter === "luxury" && "selected"}`}
+            onClick={() => handleFilterChange("type", "luxury")}
+          >
+            Luxury
+          </p>
+          <p
+            className={`filter rugged ${typeFilter === "rugged" && "selected"}`}
+            onClick={() => handleFilterChange("type", "rugged")}
+          >
+            Rugged
+          </p>
+          {typeFilter && (
+            <p
+              className="clear-filter"
+              onClick={() => handleFilterChange("type", null)}
+            >
+              Clear filters
+            </p>
+          )}
+        </div>
+        <div className="vans-container">{vans}</div>
+      </>
+    );
+  }
+
+  return (
     <div className="vans">
       <h2>Explore our van options</h2>
-      <div className="filters">
-        <p
-          className={`filter simple ${typeFilter === "simple" && "selected"}`}
-          onClick={() => handleFilterChange("type", "simple")}
-        >
-          Simple
-        </p>
-        <p
-          className={`filter luxury ${typeFilter === "luxury" && "selected"}`}
-          onClick={() => handleFilterChange("type", "luxury")}
-        >
-          Luxury
-        </p>
-        <p
-          className={`filter rugged ${typeFilter === "rugged" && "selected"}`}
-          onClick={() => handleFilterChange("type", "rugged")}
-        >
-          Rugged
-        </p>
-        {typeFilter && (
-          <p
-            className="clear-filter"
-            onClick={() => handleFilterChange("type", null)}
-          >
-            Clear filters
-          </p>
-        )}
-      </div>
-      <div className="vans-container">{vans}</div>
+      <Suspense fallback={<h2>Loading vans...</h2>}>
+        <Await resolve={loaderData.vans}>{renderVanElements}</Await>
+      </Suspense>
     </div>
-  ) : (
-    <Loading />
   );
 }
